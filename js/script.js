fe8c724bfc3a8f57d47d8c3497ee8ad8b41dc6d0
AradoChang <89583931+AradoChang@users.noreply.github.com>
2021-12-09 09:23:44 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
+$(document).ready(function(){
+    let modal = $('.modal');    
 
+    let modal_cont = $('.modal-cont');
+
+    let modal_close = $('.modal-close');
+
+    modal_close.click(function(){
+        modal.fadeOut();
+    });
+    modal.click(function(){
+        modal.fadeOut();
+    });
+    modal_cont.click(function(event) {
+        event.stopPropagation();
+    });
+});
